ae379121637eb47d2bb4c63a67439c5713746f26
piyush2040 <51694158+piyush2040@users.noreply.github.com>
2024-12-29 17:10:59 +0530

    resolved mongodb field name bug

--- Microservices-main/Rating/src/main/java/com/micro/service/Rating/Entities/Rating.java
+++ Microservices-main/Rating/src/main/java/com/micro/service/Rating/Entities/Rating.java
@@ -2,6 +2,8 @@ package com.Micro.Service.Rating.Entities;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +19,7 @@ import lombok.ToString;
 public class Rating {
     @Id
     private String id;      // MongoDB's default "_id" field
+    @Field("UserId")
     private String userId;  // Ensure case matches repository method
     private String hotelId;
     private int rating;
